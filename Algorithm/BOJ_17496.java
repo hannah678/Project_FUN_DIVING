4e8d84641c93d255524fce25397bb985be8aca15
greenarry <96934758+hyeyoung-dev@users.noreply.github.com>
2022-01-11 22:50:26 +0900

    Change file name

--- Algorithm/BOJ_17496.java
+++ Algorithm/BOJ_17496.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class BOJ_17496 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int N = scanner.nextInt();
