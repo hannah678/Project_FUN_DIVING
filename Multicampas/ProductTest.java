359f36ea04df4e5c05bb500db13e0e65936f7f54
greenarry <96934758+hyeyoung-dev@users.noreply.github.com>
2022-01-13 15:34:41 +0900

    Add: arrays & for loop

--- Multicampas/ProductTest.java
+++ Multicampas/ProductTest.java
@@ -1,36 +1,26 @@
 class Product {
 	
-	int prdNo;
-	String prdName;
-	int prdPrice;
-	int prdYear;
-	String prdMaker;
-	
-	public Product(int prdNo, String prdName, int prdPrice, int prdYear, String prdMaker) {
-		this.prdNo = prdNo;
-        this.prdName = prdName;
-        this.prdPrice = prdPrice;
-        this.prdYear = prdYear;
-        this.prdMaker = prdMaker;
-	}
+	int[] prdNo = {1, 2, 3};
+	String[] prdName = {"노트북", "모니터", "마우스"};
+	int[] prdPrice = {1200000, 300000, 30000};
+	int[] prdYear = {2021, 2021, 2020};
+	String[] prdMaker = {"삼성", "LG", "로지텍"};
 	
 	public String toString(){
-        return String.format("%03d\t%s\t%d\t%d\t%s%n", prdNo, prdName, prdPrice, prdYear, prdMaker);
-	}
+	     String result = "";
+	      for(int i=0; i<prdNo.length; i++) {
+	         result += String.format("%03d\t%s\t%d\t%d\t%s%n", prdNo[i], prdName[i], prdPrice[i], prdYear[i], prdMaker[i]);
+	      }
+	     return result;
+	   }
 }
 
 public class ProductTest {
 	public static void main(String[] args) {
-		Product p1 = new Product(1, "노트북", 1200000, 2021, "삼성");
-	    Product p2 = new Product(2, "모니터", 300000, 2021, "LG");
-	    Product p3 = new Product(3, "마우스", 30000, 2020, "로지텍");
-		
+		Product p = new Product();
+	    
 		System.out.println("상품번호\t상품명\t가격\t연도\t제조사");
 		System.out.println("-------------------------------------");
-		
-		System.out.println(p1.toString());
-		System.out.println(p2.toString());
-		System.out.println(p3.toString());
+		System.out.println(p.toString());
 	}
 }
-
